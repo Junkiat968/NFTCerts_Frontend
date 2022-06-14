845989d9033c3adbe006d3a72bfd2f75c889a750
sxbrinx <smxlik@gmail.com>
2022-06-14 08:19:33 +0800

    Update 1
    Added navigation bar and various pages

---
+++ src/components/pages/purchaseTokens.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const purchaseTokens = () => {
+    return (
+        <div>
+            <small> testing testing </small>
+        </div>
+    )
+};
+
+export default purchaseTokens;
---
+++ src/components/pages/transactionLogs.js
@@ -0,0 +1,28 @@
+import './transactionLogs.css';
+import React from 'react';
+
+function transactionLogs () {
+    return (
+        <div className='table-container'>
+            <table>
+                <thead>
+                    <tr>
+                        <th> Transaction </th>
+                        <th> Timestamp </th>
+                        <th> Tokens transacted </th>
+                    </tr>
+                </thead>                
+
+                <tbody>
+                    <tr>
+                        <td>Uploaded grades ICT1001</td>
+                        <td>Fri, 14 May 2022, 14:52:00</td>
+                        <td>100</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+};
+
+export default transactionLogs;
