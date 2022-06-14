b618f8dd4ec1b83a4207e370a9f57149a243acac
sxbrinx <smxlik@gmail.com>
2022-06-14 08:23:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import myAccount from './components/pages/myAccount';
+import ManageGrades from './components/pages/manageGrades';
+import purchaseTokens from './components/pages/purchaseTokens';
+import transactionLogs from './components/pages/transactionLogs';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        content
-      </h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' component={ManageGrades} />
+          <Route path='/transactionLogs' component={transactionLogs} />
+          <Route path='/purchaseTokens' component={purchaseTokens} />
+          <Route path='/myAccount' component={myAccount} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
