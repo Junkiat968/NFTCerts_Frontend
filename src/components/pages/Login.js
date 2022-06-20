b91eb9ab5c2a4adce1084753f5ea2413a15d4f4c
Kai Siang <2001476@sit.singaporetech.edu.sg>
2022-06-20 20:47:36 +0800

    login page styling
    add css for login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,18 +3,40 @@ import "./Login.css";
 
 export default function Login() {
   return (
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+<div class="overlay">
+<form>
+   <div class="con">
+   <header class="head-form">
+      <h2>Log In</h2>
+   </header>
+   <br/>
+   <div class="field-set">
+     
+         <span class="input-item">
+           <i class="fa fa-user-circle"></i>
+         </span>
+         <input class="form-input" id="txt-input" type="text" placeholder="Employee/Student ID" required />
+     
+      <br/>
+     
+     
+      <span class="input-item">
+        <i class="fa fa-key"></i>
+       </span>
+      <input class="form-input" type="password" placeholder="Password" id="pwd"  name="password" required />     
+     
+      <br/>
+      <button class="log-in">Sign In</button>
+   </div>
+  
+   <div class="other">
+      <button class="btn submits forget-pass">Forgot Password</button>
+   </div>
+     
+  </div>
+  
+</form>
+</div>
+
   )
 }
