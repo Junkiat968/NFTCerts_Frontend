d6a303e42c0e4bcc9085b45cb85cf96894328038
Kai Siang <2001476@sit.singaporetech.edu.sg>
2022-06-20 13:16:28 +0800

    upload button styling
    add css styling for upload button
    align right

--- src/components/pages/manageGrades.js
+++ src/components/pages/manageGrades.js
@@ -1,52 +1,63 @@
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import Sidebar from './Sidebar';
-import './manageGrades.css';
+import Sidebar from "./Sidebar";
+import "./manageGrades.css";
 
 const ManageGrades = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    const readExcel = (file) => {
-      const promise = new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.readAsArrayBuffer(file);
-  
-        fileReader.onload = (e) => {
-          const bufferArray = e.target.result;
-  
-          const wb = XLSX.read(bufferArray, { type: "buffer" });
-  
-          const wsname = wb.SheetNames[0];
-  
-          const ws = wb.Sheets[wsname];
-  
-          const data = XLSX.utils.sheet_to_json(ws);
-  
-          console.log(data);
-  
-          resolve(data);
-        };
-  
-        fileReader.onerror = (error) => {
-          reject(error);
-        };
-      });
-  
-      promise.then((d) => {
-        setItems(d);
-      });
-    };
-  
-    return (
-      <div>
-        <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
 
-        <input type="file" onChange={(e) => {const file = e.target.files[0]; 
-        readExcel(file);}}
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        console.log(data);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
+
+  return (
+    <div>
+      <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+
+      <div class="div-wrapper"> 
+        <input
+          id="uploadGrade"
+          type="file"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            readExcel(file);
+          }}
+          hidden
         />
+        <label id="uploadGradeBtn" for="uploadGrade">
+          Upload Grades
+        </label>
+      </div>
 
-        <div class="table-container">
-          <table>
+      <div class="table-container">
+        <table>
           <thead>
             <tr>
               <th scope="col">Student</th>
@@ -64,10 +75,10 @@ const ManageGrades = () => {
               </tr>
             ))}
           </tbody>
-          </table>
-        </div>
+        </table>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ManageGrades;
