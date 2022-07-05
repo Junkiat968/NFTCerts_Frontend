062acd8f5edeacdc75966f08a08e445e27f1e2d7
Jaspzx <jaspzx@hotmail.com>
2022-07-05 19:26:05 +0800

    Test Routing Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { EthProvider, EthContext } from "./contexts/EthContext";
 import "./App.css";
 
