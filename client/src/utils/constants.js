72699f05ed48a8f933845135b4a5cb5086c88155
Jaspzx <jaspzx@hotmail.com>
2022-07-05 17:29:42 +0800

    Updated With New SITNFT Contract

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -4,5 +4,5 @@ import base64Abi from "../contracts/Base64.json";
 export const base64ContractAddress = '0x03101b20aAFc81b35D87aC59D679a8c8810ECedc';
 export const base64ContractABI = base64Abi.abi;
 
-export const sitnftContractAddress = '0x42308D1ceB2C41EcB7cD78b7fA46B47309782014';
+export const sitnftContractAddress = '0x96b06c9bd32f30e064e599671893F966743Dcef4';
 export const sitnftContractABI = sitnftAbi.abi;
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,7 @@
-// var ss = artifacts.require("./SimpleStorage.sol");
 var b64 = artifacts.require("./Base64.sol");
-// var rc = artifacts.require("./RoleControl.sol");
 var sitnft = artifacts.require("./SITNFT.sol");
 
 module.exports = function (deployer) {
-  // deployer.deploy(ss);
-  deployer.deploy(b64);
+  // deployer.deploy(b64);
   deployer.deploy(sitnft);
 };
