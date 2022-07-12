9e6f3595b10427e92b66061a7dc901a5b0903382
Jaspzx <jaspzx@hotmail.com>
2022-07-12 23:41:10 +0800

    Redeployed SITNFT Contract

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -4,5 +4,5 @@ import base64Abi from "../contracts/Base64.json";
 export const base64ContractAddress = '0x03101b20aAFc81b35D87aC59D679a8c8810ECedc';
 export const base64ContractABI = base64Abi.abi;
 
-export const sitnftContractAddress = '0x6C9B5F8a2aDd0f2E951E3a3151784C74B69BD9E2';
+export const sitnftContractAddress = '0x683B0036F269D6Ac65498C48Dafd1F549ca2b02f';
 export const sitnftContractABI = sitnftAbi.abi;
